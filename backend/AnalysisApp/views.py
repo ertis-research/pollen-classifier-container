30f413210f51ee4354ce561a5647ef9297e0bc84
Antonio Jesús <chaves@uma.es>
2023-05-23 13:27:40 +0000

    refactor (backend): Upgraded dependencies (SimpleJWT, Django, DRF and ADRF) and async views.

--- backend/AnalysisApp/views.py
+++ backend/AnalysisApp/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import permission_classes #, api_view
+from adrf.decorators import api_view # as async_api_view
 from rest_framework.permissions import IsAuthenticated
 
 from rest_framework.parsers import JSONParser
@@ -17,24 +18,29 @@ from pathlib import Path
 
 import numpy as np
 import zipfile
-import requests
+import httpx as requests
 import os
 
 from collections import OrderedDict
+
+from asgiref.sync import sync_to_async
 
 import jwt
 
 # Create your views here.
 
-def getUserDetails_fromJWT(request):
-    jwt_token = request.META['HTTP_AUTHORIZATION'][4:]
-    jwt_token_decoded = jwt.decode(str(jwt_token), SECRET_KEY, algorithms="HS256")    
+# @sync_to_async
+async def getUserDetails_fromJWT(request):
+    jwt_token = request.META['HTTP_AUTHORIZATION'][len("Bearer "):]
+    jwt_token_decoded = jwt.decode(str(jwt_token), SECRET_KEY, algorithms="HS256")
+
     UserId = jwt_token_decoded['user_id']
     username = jwt_token_decoded['username']
     email = jwt_token_decoded['email']
     
     try:
-        User.objects.get(id=UserId, username = username, email = email)
+        await User.objects.aget(id=UserId, username = username, email = email)
+        # User.objects.get(id=UserId, username = username, email = email)
     except:
         return -1, 401 # Unauthorized
 
@@ -43,18 +49,18 @@ def getUserDetails_fromJWT(request):
 
 @api_view(['GET', 'DELETE'])
 @permission_classes([IsAuthenticated])
-def polenApi(request, id=0):
+async def polenApi(request, id=0):
     """
     This view is used by the frontend module to obtain 
     the sample analysis list or delete one analysis from the list.
 
     """    
-    user, code = getUserDetails_fromJWT(request)
+    user, code = await getUserDetails_fromJWT(request)
     if code == 401:
         return JsonResponse([], safe=False)
 
     if request.method=='GET':  
-        polen = Polen.objects.filter(UserId=user)
+        polen = await Polen.objects.filter(UserId=user)
         polen_serializer = PolenSerializer(polen, many=True)
         return JsonResponse(polen_serializer.data, safe=False)
 
@@ -69,15 +75,16 @@ def polenApi(request, id=0):
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def uploadVSI(request):
+async def uploadVSI(request):
     """
     This view is used by the frontend module to send to the backend the file with the sample to analyse.
 
-    """
-    _, code = getUserDetails_fromJWT(request)
+    """    
+    _, code = await getUserDetails_fromJWT(request)
     if code == 401:
         return JsonResponse([], safe=False)
 
+    # save the file in the server (async function)
     file=request.FILES['uploadedFile']    
     file_name = default_storage.save(file.name, file)
     file_path = default_storage.path(file_name)
@@ -106,67 +113,25 @@ def uploadVSI(request):
         responseList.append(images)    
     return JsonResponse(responseList, safe=False)
 
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def analyseSelectedImages_old(request):
+async def analyseSelectedImages(request):
     """
     This view is used by the frontend module to send to the backend how it has to analyse the image previously sent.
 
     """
-    userId, code = getUserDetails_fromJWT(request)
+    userId, code = await getUserDetails_fromJWT(request)
     if code == 401:
-        return JsonResponse("Analysis Failed", safe=False)
+        return JsonResponse("Analysis Failed - JWT", safe=False)
 
     reqStr  = JSONParser().parse(request)
     if reqStr['UserId'] != userId:        
-        return JsonResponse("Analysis Failed", safe=False)
+        return JsonResponse("Analysis Failed - Auth", safe=False)
     
     parsed_route = os.path.join(reqStr['PhotoFilePath'], "parsed")    
 
-    af.parseImages(reqStr['SelectedRowsIds'], os.path.join(reqStr['PhotoFilePath'], reqStr['PhotoFileName']), parsed_route)
-    
-    totalBlobsDetected = [0,0]   
-    if (len(reqStr['SelectedRowsIds']) != 0):
-        imageList = af.listFiles(parsed_route, 'ome.tiff')
-        for image in imageList:
-            res = af.analyseImage(image, parsed_route, 0.5833) 
-            totalBlobsDetected = np.add(totalBlobsDetected, res )
-        
-    
-    new_pollen = OrderedDict()
-    new_pollen['AnalysisId'] = reqStr['AnalysisId']
-    new_pollen['AnalysisName'] = reqStr['AnalysisName']
-    new_pollen['SampleDate'] = reqStr['SampleDate']
-    new_pollen['AnalysisDate'] = reqStr['AnalysisDate']
-    new_pollen['PhotoFileName'] = reqStr['PhotoFileName']
-    new_pollen['PhotoFilePath'] = reqStr['PhotoFilePath']
-    new_pollen['UserId'] = reqStr['UserId']
-    new_pollen['AnalysisResult']  = res.headers['AnalysisResult']
-
-    polen_serializer = PolenSerializer(data= new_pollen)
-    if polen_serializer.is_valid():
-        polen_serializer.save()
-        return JsonResponse("Analysis Okay", safe=False)
-    return JsonResponse("Analysis Failed", safe=False)
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def analyseSelectedImages(request):
-    """
-    This view is used by the frontend module to send to the backend how it has to analyse the image previously sent.
-
-    """
-    userId, code = getUserDetails_fromJWT(request)
-    if code == 401:
-        return JsonResponse("Analysis Failed", safe=False)
-
-    reqStr  = JSONParser().parse(request)
-    if reqStr['UserId'] != userId:        
-        return JsonResponse("Analysis Failed", safe=False)
-    
-    parsed_route = os.path.join(reqStr['PhotoFilePath'], "parsed")    
-
-    af.parseImages(reqStr['SelectedRowsIds'], os.path.join(reqStr['PhotoFilePath'], reqStr['PhotoFileName']), parsed_route)
+    await af.parseImages(reqStr['SelectedRowsIds'], os.path.join(reqStr['PhotoFilePath'], reqStr['PhotoFileName']), parsed_route)
     
     if (len(reqStr['SelectedRowsIds']) > 0):        
         imageList = af.listFiles(parsed_route, 'ome.tiff')
@@ -177,7 +142,7 @@ def analyseSelectedImages(request):
 
         zip_file.close()
 
-        res = requests.post(f'{YOLO_EXECUTOR_URL}/predict', files={'images.zip': open(zip_path, 'rb')})
+        res = await requests.post(f'{YOLO_EXECUTOR_URL}/predict', files={'images.zip': open(zip_path, 'rb')})
 
     new_pollen = OrderedDict()
     new_pollen['AnalysisId'] = reqStr['AnalysisId']
@@ -189,8 +154,8 @@ def analyseSelectedImages(request):
     new_pollen['UserId'] = reqStr['UserId']
     new_pollen['AnalysisResult']  = res.headers['AnalysisResult']
 
-    polen_serializer = PolenSerializer(data= new_pollen)
+    polen_serializer = PolenSerializer(data=new_pollen)
     if polen_serializer.is_valid():
         polen_serializer.save()
         return JsonResponse("Analysis Okay", safe=False)
-    return JsonResponse("Analysis Failed", safe=False)
+    return JsonResponse("Analysis Failed - Error", safe=False)
