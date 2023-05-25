68f7c64c0bb41ad5a5bb26e1ee85dc9ea6f20192
Antonio Jesús <chaves@uma.es>
2023-05-25 10:48:42 +0000

    feat (backend/admin.py): Added Polen view to admin site.

--- backend/AnalysisApp/admin.py
+++ backend/AnalysisApp/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 # Register your models here.
+
+from AnalysisApp.models import Polen
+
+class PolenAdmin(ModelAdmin):
+    list_display = ('AnalysisId', 'UserId', 'AnalysisName', 'AnalysisDate', 'SampleDate', 'PhotoFileName', 'PhotoFilePath', 'AnalysisResult')
+
+admin.site.register(Polen, PolenAdmin)
